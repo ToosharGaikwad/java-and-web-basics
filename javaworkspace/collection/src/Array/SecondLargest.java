259395ebe18c581913fe73e9d173278751cd2d7a
Tushar <gaikwadtushar@gmail.com>
2026-04-18 11:05:24 +0530

    correct a correction

--- javaworkspace/collection/src/Array/SecondLargest.java
+++ javaworkspace/collection/src/Array/SecondLargest.java
@@ -2,21 +2,21 @@ package Array;
 
 public class SecondLargest {
 
-	int []arr = { 33,1,55,66,4};
+	
 	
 	public static int slarge(int[] arr)
 	{
 		int large=Integer.MIN_VALUE;
 		int secondLarge = Integer.MIN_VALUE;
 		
-		for(int i=0;i<arr.length-1;i++)
+		for(int i=0;i<arr.length;i++)
 		{
-			if(large>arr[i])
+			if(arr[i]>large)
 			{
 				secondLarge = large;
 				large = arr[i];
 				
-			}else if(arr[i]>large&&arr[i] != large)
+			}else if(arr[i]>secondLarge&&arr[i] != large)
 			{
 				secondLarge = arr[i];
 			}
@@ -24,6 +24,8 @@ public class SecondLargest {
 		return secondLarge;
 	}
 	public static void main(String[] args) {
+		int []arr = { 33,1,5,5,79};
+		System.out.println(slarge(arr));
 		
 	}
 }
